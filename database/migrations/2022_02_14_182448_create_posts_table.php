470192030c10eedc526e7007841b44b409abd445
Carlos Espejel <carlosespejel71@gmail.com>
2022-02-14 13:21:58 -0600

    Model físico de la bd y relación a nivel de modelos

---
+++ database/migrations/2022_02_14_182448_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug')->unique();
+
+            $table->text('extract');
+            $table->longText('body');
+
+            $table->enum('status', [Post::DRAFT, Post::PUBLISHED])->default(Post::DRAFT);
+
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
