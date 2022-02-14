077116b5e26bb286eca902812600e0b0ecdb2e01
Carlos Espejel <carlosespejel71@gmail.com>
2022-02-14 17:34:19 -0600

    Login controller

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        if (Hash::check($request->password, $user->password)) {
+            return UserResource::make($user);
+        } else {
+            return response()->json([
+                'message' => 'These credentials do not match our records'
+            ], 404);
+        }
+    }
+}
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\LoginController;
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [RegisterController::class, 'register'])->name('api.v1.register');
+Route::post('login', [LoginController::class, 'login'])->name('api.v1.login');
 
 Route::apiResource('categories', CategoryController::class)->names('api.v1.categories');
 Route::apiResource('posts', PostController::class)->names('api.v1.posts');
