ea2e22e3a7237d5539315e514b7251e646df1ee7
Carlos Espejel <carlosespejel71@gmail.com>
2022-02-14 18:40:57 -0600

    Encriptar contraseñas en el registro

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -16,7 +16,11 @@ class RegisterController extends Controller
             'password' => 'required|string|min:8|confirmed',
         ]);
 
-        $user = User::create($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
 
         return response($user, 200);
     }
